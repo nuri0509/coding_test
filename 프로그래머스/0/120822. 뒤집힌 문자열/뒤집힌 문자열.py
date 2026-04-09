f2c1677cf6a27c2ab6a24a1093871d260655bfc5
Nuri Lee <149138916+nuri0509@users.noreply.github.com>
2026-04-09 21:17:56 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.00 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.py
@@ -0,0 +1,19 @@
+def solution(my_string):
+
+    my_list = list(my_string)
+    my_list.reverse()
+
+    answer = ''.join(my_list)
+
+    return answer
+
+'''
+def solution(my_string):
+
+    my_list = list(my_string)
+    my_list.reverse()
+
+    answer = ''.join(my_list)
+
+    return answer
+'''
